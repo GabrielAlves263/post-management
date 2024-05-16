b1bf042587ef4d5a6a28c51dbf3624e3d44d4fcc
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2024-05-16 15:01:32 -0300

    feat(api): Standardize API response format

--- src/controllers/postagens.controller.js
+++ src/controllers/postagens.controller.js
@@ -1,6 +1,7 @@
 import { mysql } from "../database/mysql.js";
 
 export default class PostagensController {
+
     getAll(req, res, next) {
         mysql.getConnection((error, conn) => {
             if (error) { return res.status(500).send({ error: error }) }
@@ -12,9 +13,26 @@ export default class PostagensController {
 
                     if (error) { return res.status(500).send({ error: error }) }
 
-                    res.status(200).send({
-                        response: result
-                    })
+                    const response = {
+                        length: result.length,
+                        posts: result.map(post => {
+                            return {
+                                id: post.id,
+                                title: post.title,
+                                descripition: post.descripition,
+                                created_at: post.created_at,
+                                updated_at: post.updated_at,
+                                type: post.type,
+                                request: {
+                                    type: 'GET',
+                                    descripition: 'Get a post by id',
+                                    url: 'http://localhost:3000/postagens/' + post.id
+                                }
+                            }
+                        })
+                    }
+
+                    return res.status(200).send(response)
                 }
             )
 
@@ -33,9 +51,29 @@ export default class PostagensController {
 
                     if (error) { return res.status(500).send({ error: error }) }
 
-                    res.status(200).send({
-                        response: result
-                    })
+                    if (result.length == 0) {
+                        return res.status(404).send({
+                            message: `No post with id ${req.params.id} was found`
+                        })
+                    }
+
+                    const response = {
+                        post: {
+                            id: result[0].id,
+                            title: result[0].title,
+                            descripition: result[0].descripition,
+                            created_at: result[0].created_at,
+                            updated_at: result[0].updated_at,
+                            type: result[0].type,
+                            request: {
+                                type: 'GET',
+                                descripition: 'Get all posts',
+                                url: 'http://localhost:3000/postagens'
+                            }
+                        }
+                    }
+
+                    return res.status(200).send(response)
                 }
             )
         })
@@ -54,10 +92,24 @@ export default class PostagensController {
 
                     if (error) { return res.status(500).send({ error: error }) }
 
-                    res.status(201).send({
-                        mensagem: 'Postagem inserida com sucesso!',
-                        id_postagem: result.insertId
-                    })
+                    const response = {
+                        message: 'Post inserted successfully!',
+                        post: {
+                            id: result.id,
+                            title: req.body.titulo,
+                            descripition: req.body.descricao,
+                            created_at: req.body.criado,
+                            updated_at: req.body.atualizado,
+                            type: req.body.tipo,
+                            request: {
+                                type: 'GET',
+                                descripition: 'Get all posts',
+                                url: 'http://localhost:3000/postagens'
+                            }
+                        }
+                    }
+
+                    return res.status(201).send(response)
                 }
             )
         })
@@ -79,10 +131,23 @@ export default class PostagensController {
 
                     if (error) { return res.status(200).send({ error: error }) }
 
-                    res.status(202).send({
-                        mensagem: 'Postagem atualizada com sucesso!',
-                        id_postagem: result.insertId
-                    })
+                    const response = {
+                        message: 'Post updated successfully!',
+                        post: {
+                            id: result.id,
+                            title: req.body.titulo,
+                            descripition: req.body.descricao,
+                            updated_at: req.body.atualizado,
+                            type: req.body.tipo,
+                            request: {
+                                type: 'GET',
+                                descripition: 'Get a post by id',
+                                url: 'http://localhost:3000/postagens/' + req.params.id
+                            }
+                        }
+                    }
+
+                    return res.status(202).send(response)
                 }
             )
         })
@@ -100,10 +165,21 @@ export default class PostagensController {
 
                     if (error) { return res.status(500).send({ error: error }) }
 
-                    res.status(202).send({
-                        mensagem: 'Postagem deletada com sucesso!',
-                        id_postagem: result.insertId
-                    })
+                    const response = {
+                        message: "Post deleted successfully!",
+                        request: {
+                            type: 'POST',
+                            descripition: 'Insert new post',
+                            url: 'http://localhost:3000/postagens',
+                            body: {
+                                title: "String",
+                                descripition: "String",
+                                typo: "String"
+                            }
+                        }
+                    }
+
+                    return res.status(202).send(response)
                 }
             )
         })
